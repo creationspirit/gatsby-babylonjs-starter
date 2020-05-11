2cd153e9c1d0f83148222ffd9051c3d5cd10a9d8
Andrija Perusic <aperusic18@gmail.com>
2020-05-11 13:32:22 +0200

    Add babylon to default starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Babylon Starter`,
+    description: `Kick off your next, great Gatsby-Babylon project with this starter.`,
     author: `@gatsbyjs`,
   },
   plugins: [
---
+++ src/components/content.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./content.css"
+
+const Content = () => (
+  <div className="content">
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby-Babylon site.</p>
+    <p>Click on the rotating box and go build something great.</p>
+    <p>You can use mouse and arrow keys to move around.</p>
+  </div>
+)
+
+export default Content
